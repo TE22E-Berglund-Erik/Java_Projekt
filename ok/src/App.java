8aefc379c2f18301ccc17555080480299eb18e0f
TE22E-Erik-Berglund <erik.berglund2@elev.ga.ntig.se>
2023-09-04 08:59:59 +0200

    pluh

---
+++ ok/src/App.java
@@ -0,0 +1,20 @@
+
+public class App {
+    public static void main(String[] args)  {
+    
+        System.out.println("______________________________" );
+        System.out.println("___________creepbrah__________" );
+        System.out.println("______________________________" );
+        System.out.println("______######______######______" );
+        System.out.println("______######______######______" );
+        System.out.println("____________######____________" );
+        System.out.println("_________############_________" );
+        System.out.println("_________############_________" );
+        System.out.println("_________###______###_________" );
+        System.out.println("_________###______###_________" );
+        System.out.println("______________________________" );
+        System.out.println("_______Pelle__Paltnacke_______" );
+        System.out.println("______________________________" );
+        
+    }
+}
